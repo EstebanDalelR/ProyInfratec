a85677aa1be18d96fa5a6383a8f11ac2d3a268a2
julio1023 <ja.poveda10@uniandes.edu.co>
2015-10-11 12:14:10 -0500

    Update 11 metodo leerMuestreo

--- main.c
+++ main.c
@@ -128,9 +128,9 @@ unsigned short leerMuestreo( unsigned short * pista, int bitpos, int bitsPorMues
 	unsigned short respuesta[15] = 0;
 	int numMuestreos = sizeof(pista);
 	int numTotalBits = numMuestreos*bitsPorMuestreo;
-	int m;
-	int i;
-	int j;
+	int m=0;
+	int i=0;
+	int n=0;
 	
 	//Recorremos el numero total de bits 
 	for (int i = 0; i < numTotalBits; i++)
@@ -141,7 +141,8 @@ unsigned short leerMuestreo( unsigned short * pista, int bitpos, int bitsPorMues
 			//Este for nos permite manejar la posicion de los bits desde bitpos hasta completar un muestreo
 			for (m = bitpos; m < bitsPorMuestreo; m++)
 			{
-				respuesta[i] = pista[m];
+				respuesta[n] = pista[m];
+				n++;
 			}
 			//j no es una variable válida
 			//la cambié por i
