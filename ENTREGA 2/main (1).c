8385d4fb54db8677370c0789342fd785e59e667c
Esteban Dalel R <estebandalelr@gmail.com>
2015-11-17 12:11:26 -0500

    copiar código a leerMuestreo

--- ENTREGA 2/main (1).c
+++ ENTREGA 2/main (1).c
@@ -170,7 +170,23 @@ unsigned short leerMuestreo( unsigned short * pista, int bitpos, int bitsPorMues
 	//-----------------------------------------------------------------------------------
 	_asm
 	{
+		//se hace push de los registros a usar
+		push eax
+		push ebx
+		push ecx
 
+		//la variable de respuesta será eax
+		mov  eax , 0	   //limpia eax
+		mov  ah  , bit     //captura en ah el valor de bit
+		mov  ebx , bitpos  //captura en ebx el valor de bitpos
+		mov  ecx , pista   //captura en ecx el valor de pista (el apuntador)
+		add  ecx , ebx 	   //le suma al apuntador de ecx el valor de ebx para llegar a la posición a cambiar
+		mov [ecx], ah	   //mueve a la posición apuntada por ecx el valor en ah
+
+		//se hace pop de los registros usados
+		pop ecx
+		pop ebx
+		pop eax
 	}
 } 
 
